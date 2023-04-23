e7b655ec2710a51f17aaf3f74ba3cc9642bab8af
Hưng Nguyễn <hung131119782002@gmail.com>
2023-04-24 03:33:42 +0700

    add footer login register for mobile n desktop

--- fe-g21/src/components/Navbar.jsx
+++ fe-g21/src/components/Navbar.jsx
@@ -3,47 +3,47 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="w-full p-2 bg-gray-400 flex justify-between">
-      <NavLink className="px-3 py-2 text-white bg-blue-300 rounded-md" to="/">
+      <NavLink className="px-2 py-1 text-white bg-blue-300 rounded-md" to="/">
         Poketo
       </NavLink>
-      <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+      {/* <NavLink
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/shopp-all"
       >
         Shop All
       </NavLink>
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/best-sellers"
       >
         Best Sellers
       </NavLink>
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/notebooks-planners"
       >
         Notebooks & Planners
       </NavLink>
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/desk-supplies"
       >
         Desk Supplies
       </NavLink>
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/home-lifestyle"
       >
         Home & Lifestyle
       </NavLink>
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/login"
       >
         Login
-      </NavLink>
+      </NavLink> */}
       <NavLink
-        className="px-3 py-2 text-white bg-blue-300 rounded-md"
+        className="px-2 py-1 text-white bg-blue-300 rounded-md"
         to="/cart"
       >
         Cart
