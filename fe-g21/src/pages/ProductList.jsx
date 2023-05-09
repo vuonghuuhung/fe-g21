ea78d5c03eb47cb347ecfe09210d6e8f14721522
vuonghuuhung <vuonghuuhung2002@gmail.com>
2023-05-09 21:49:49 +0700

    G21-40 #add product-list

--- fe-g21/src/pages/ProductList.jsx
+++ fe-g21/src/pages/ProductList.jsx
@@ -1,30 +1,60 @@
-export default function ProductList({ products = null, categoryName="Best Sellers"}) {
-  const categorizedProducts = products !== null ? products : null;
-  console.log(products, categoryName);
+import { useLocation } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
+
+export default function ProductList() {
+  const { state } = useLocation();
+  const products = state.products;
+  const categoryName = state.categoryName;
+  const imgCategories = {
+    shopAll:
+      "https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510",
+    bestSellers:
+      "https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510",
+    notebooks:
+      "https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510",
+    deskSupplies:
+      "https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510",
+    homeAndLifestyles:
+      "https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510",
+  };
+  let imgUrl = "";
+  switch (categoryName) {
+    case "Shop All":
+      imgUrl = imgCategories.shopAll;
+      break;
+    case "Best Sellers":
+      imgUrl = imgCategories.bestSellers;
+      break;
+    case "Notebooks & Planners":
+      imgUrl = imgCategories.notebooks;
+      break;
+    case "Desk Supplies":
+      imgUrl = imgCategories.deskSupplies;
+      break;
+    case "Home & Lifestyle":
+      imgUrl = imgCategories.homeAndLifestyles;
+      break;
+
+    default:
+      imgUrl = imgCategories.shopAll;
+      break;
+  }
 
   return (
     <>
-      <div className="flex h-96 w-full bg-[url('https://cdn.shopify.com/s/files/1/0001/5211/collections/BestSellers-Category-Banner_2400x.jpg?v=1607034510')]">
+      <div className={"flex h-96 w-full bg-[url('" + imgUrl + "')]"}>
         <h1 className="m-auto text-white text-8xl font-semibold">
           {categoryName}
         </h1>
       </div>
-      <div class="bg-white">
-        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {categorizedProducts !== null ? categorizedProducts.map((product) => {
-              <a href="#" class="group">
-                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                  <img
-                    src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
-                    alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                    class="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
-                </div>
-                <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-                <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-              </a>;
-            }) : null}
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-full lg:px-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products !== null
+              ? products.map((product, index) => (
+                  <ProductCard product={product} />
+                ))
+              : null}
           </div>
         </div>
       </div>
