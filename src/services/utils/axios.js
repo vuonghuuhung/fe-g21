6950a31ef10644cbbbc96614384cb02524dcbf25
vuonghuuhung <vuonghuuhung2002@gmail.com>
2023-06-27 07:16:14 +0700

    fix

--- src/services/utils/axios.js
+++ src/services/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "https://g21-be.azurewebsites.net/",
+    baseURL: "https://g21-be.azurewebsites.net/",
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
